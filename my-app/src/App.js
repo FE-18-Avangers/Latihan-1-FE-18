bcb59c245462326650039b8a3f95dd0f508366c1
andiaditya21 <andiaditya21@gmail.com>
2020-11-04 12:17:50 +0800

    login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import Login from "./components/Login";
+import { BrowserRouter, Route } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Selamat Datang FE-18-Avangers</h1>
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Login} />
+      <Route path="/register" component={Register} />
+    </BrowserRouter>
   );
 }
 
---
+++ my-app/src/components/Login.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Img_1 from "../img/img_1.jpg";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <>
+        <div className="container" style={{ marginTop: 60 }}>
+          <h2 className="text-center mb-5">Login</h2>
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src={Img_1}
+                className="img-fluid"
+                alt="Responsive image"
+                width="560"
+                style={{ borderRadius: 4 }}
+              />
+            </div>
+            <div className="col-md-6 align-self-center my-3">
+              <form>
+                <div className="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Fill your Email, Please"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Fill your Password, Please"
+                    required
+                  />
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    class="btn btn-primary text-center px-5"
+                  >
+                    Sign in
+                  </button>
+                  <p>
+                    Belum punya akun? <Link to="/register"> Kesini segera</Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
