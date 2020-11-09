68df4e066ff7f9b93a6219c7e0014e9fadd93081
petrusdarius <petrusdarius@gmail.com>
2020-11-09 10:05:27 +0700

    link homepage

--- my-app/src/components/API/movie.js
+++ my-app/src/components/API/movie.js
@@ -64,7 +64,10 @@ class Movie extends Component {
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </div>
-                {listfilm}
+                <div className='listfilm'>
+                    {listfilm}
+                </div>
+                
             </div>
         )
     }
