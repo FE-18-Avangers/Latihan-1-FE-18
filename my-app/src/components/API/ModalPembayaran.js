d496225356c1e99b767e67c6989ea5b305887b49
Dedi Irawan <dediirwansyah1925@gmail.com>
2020-11-09 21:29:12 +0700

    menambah file pembayaran dan modifikasi modalPembayaran

--- my-app/src/components/API/ModalPembayaran.js
+++ my-app/src/components/API/ModalPembayaran.js
@@ -1,23 +1,43 @@
-import logo from './payment.svg';
-import './styleD.css';
+import React, { Component } from 'react'
+import '../../styleD.css';
+import logo from '../../payment.svg';
+import { Link } from 'react-router-dom';
 
-function ModalPembayaran() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button type="submit">Logo</button>
-        <button type="submit">Keluar</button>
-      </header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='utama'>Pemesanan berhasil </p>
-        <p>Silahkan cek email untuk melakukan pembayaran </p>
-        <button type="submit">Back To Home</button>
-        <footer>
-          <p>2020 by: FE-18</p>
-        </footer>
-    </div>
-
-  );
+class ModalPembayaran extends Component{
+    render(){
+        return (
+      <div>
+      <nav class="navbar navbar-dark mr-auto bg-dark">
+        <button class="btn btn-danger ml-5" href="#">Logo</button>
+        <button className="btn mr-5 btn-danger" href="#">Keluar</button>
+      </nav>
+      <section>
+      <div class="container ">
+              <div className="row mt-5">
+                  <div className="col-sm-12 text-center">
+                    <img src={logo}  className=""/>
+                    <div className="mt-5" />
+                      <h2>Payment successful</h2>
+                      <p className="text-muted ">Your payment information has sent your email</p>
+                  </div>
+              </div>
+              
+              
+              </div>
+              </section>
+              <footer className='bg-dark'>
+          <div class="container text-center">
+              <div class="row">
+      <div class="col-sm-12">
+          <p className='text-light'>&copy; copyright 2020 | built by FE - 18</p>
+      </div>
+              </div>
+          </div>
+          
+      </footer>
+              </div>
+    )
+}
 }
 
 export default ModalPembayaran;
